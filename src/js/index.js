58c39688fea96f121ece2bbd36580876fa0778c2
Mateusz Meler <mateuszmeler@icloud.com>
2020-10-02 21:29:06 +0200

    feat: adding first arrow

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,21 @@
-import '../scss/main.scss';
-
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
 
 /* place your code below */
+
+const cards = [
+  "yellow",
+  "blue",
+  "violet",
+  "blueviolet",
+  "green",
+  "lightsalmon",
+  "orange",
+  "lightgreen",
+  "brown",
+  "lightblue",
+];
+
